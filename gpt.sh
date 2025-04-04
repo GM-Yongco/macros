1ea9662a78fd47cab31f450b03e8e253a94586d3
Giulio Yongco <yongcogiulio125@gmail.com>
2025-04-04 12:27:11 +0800

    cursed shortcut 💀💀💀

---
+++ gpt.sh
@@ -0,0 +1,9 @@
+# Author				: G.M. Yongco #BeSomeoneWhoCanStandByShinomiya
+# Date					: ur my date uwu
+# Description			: Code that will impress u ;)
+# Actual Description	: shortcut command to turn the tab youre on to the chatgpt home screen tab
+# HEADERS ================================================================
+
+ydotool key ctrl+l ctrl+l ctrl+l ctrl+l
+ydotool type "https://chatgpt.com/"
+ydotool key enter
